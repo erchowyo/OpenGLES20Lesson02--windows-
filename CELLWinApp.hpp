0d92290bf15202f9ab858896235bdf421900b0e2
erchowyo <erchowyo@msn.cn>
2016-01-21 16:00:38 +0800

    chuangkou

---
+++ CELLWinApp.hpp
@@ -0,0 +1,146 @@
+#pragma once
+#include <tchar.h>
+#include <Windows.h>
+
+
+
+class   CELLWinApp
+{
+public:
+    //! 实例句柄
+    HINSTANCE   _hInstance;
+    //! 窗口句柄
+    HWND        _hWnd;
+public:
+    CELLWinApp(HINSTANCE hInstance)
+        :_hInstance(hInstance)
+    {
+        //1 注册窗口类
+        WNDCLASSEX  winClass;
+        winClass.lpszClassName  =   _T("CELLWinApp");
+        winClass.cbSize         =   sizeof(winClass);
+        winClass.style          =   CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+        winClass.lpfnWndProc    =   wndProc;
+        winClass.hInstance      =   hInstance;
+        winClass.hIcon	        =   0;
+        winClass.hIconSm	    =   0;
+        winClass.hCursor        =   LoadCursor(hInstance, IDC_ARROW);
+        winClass.hbrBackground  =   (HBRUSH)GetStockObject(BLACK_BRUSH);
+        winClass.lpszMenuName   =   NULL;
+        winClass.cbClsExtra     =   0;
+        winClass.cbWndExtra     =   0;
+        RegisterClassEx(&winClass);
+    }
+
+    virtual ~CELLWinApp()
+    {
+        UnregisterClass(_T("CELLWinApp"),_hInstance);
+    }
+protected:
+    static  LRESULT CALLBACK wndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+    {
+        
+        CELLWinApp*  pThis   =   (CELLWinApp*)GetWindowLong(hWnd,GWL_USERDATA);
+        if (pThis)
+        {
+            return  pThis->onEvent(hWnd,msg,wParam,lParam);
+        }
+        if (WM_CREATE == msg)
+        {
+            CREATESTRUCT*   pCreate =   (CREATESTRUCT*)lParam;
+            SetWindowLong(hWnd,GWL_USERDATA,(DWORD_PTR)pCreate->lpCreateParams);
+        }
+        return  DefWindowProc( hWnd, msg, wParam, lParam );
+    }
+public:
+    virtual LRESULT onEvent(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+    {
+        switch (msg)
+        {
+        case WM_CLOSE:
+        case WM_DESTROY:
+            {
+                ::PostQuitMessage(0);
+            }
+            break;
+        case WM_MOUSEMOVE:
+            break;
+        case WM_LBUTTONDOWN:
+            break;
+        case WM_LBUTTONUP:
+            break;
+        case WM_RBUTTONDOWN:
+            break;
+        case WM_RBUTTONUP:
+            break;
+        case WM_KEYDOWN:
+            {
+                switch(wParam)
+                {
+                case VK_LEFT:
+                    break;
+                default:
+                    break;
+                }
+            }
+            break;
+        case WM_KEYUP:
+            break;
+        default:
+            return  DefWindowProc( hWnd, msg, wParam, lParam ); 
+        }
+        return  S_OK;
+        
+    }
+    /**
+    *   主函数
+    */
+    int     main(int width,int height)
+    {
+        _hWnd   =   CreateWindowEx( NULL,
+                                    _T("CELLWinApp"),
+                                    _T("CELLWinApp"),
+                                    WS_OVERLAPPEDWINDOW,
+                                    0,
+                                    0,
+                                    width,
+                                    height, 
+                                    0, 
+                                    0,
+                                    _hInstance, 
+                                    this
+                                    );
+
+        if (_hWnd == 0)
+        {
+            return  -1;
+        }
+
+        UpdateWindow(_hWnd);
+        ShowWindow(_hWnd,SW_MAXIMIZE);
+
+        MSG msg =   {0};
+        while(msg.message != WM_QUIT)
+        {
+            if (msg.message == WM_DESTROY || 
+                msg.message == WM_CLOSE)
+            {
+                break;
+            }
+            /**
+            *   有消息，处理消息，无消息，则进行渲染绘制
+            */
+            if( PeekMessage( &msg, NULL, 0, 0, PM_REMOVE ) )
+            { 
+                TranslateMessage( &msg );
+                DispatchMessage( &msg );
+            }
+            else
+            {
+                Sleep(1);
+            }
+        }
+        return  0;
+    }
+    
+};
